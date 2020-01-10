a01de71f37d46449884b45a844cb0faefba9c8b4
edinak26 <edinak26@gmail.com>
2020-01-11 00:21:24 +0200

    kotlin style added

--- src/main/CodeAnalyzer.kt
+++ src/main/CodeAnalyzer.kt
@@ -4,12 +4,14 @@ import java.io.File
 import java.util.*
 
 class CodeAnalyzer {
-    var lineCount = 0
     var maxLineWidth = 0
     var widestLineNumber = 0
     var totalChars = 0
     val lineWidthHistogram = LineWidthHistogram()
-    val meanLineWidth get() = totalChars.toDouble()/lineCount
+    val lineCount get() = lineWidthHistogram.lineCount
+    val meanLineWidth get() = totalChars.toDouble() / lineCount
+
+    private val sortedWidths get() = lineWidthHistogram.getWidths().sorted()
 
     fun findJavaFiles(parentDirectory: File): List<File> {
         val files = ArrayList<File>()
@@ -18,45 +20,34 @@ class CodeAnalyzer {
     }
 
     private fun findJavaFiles(parentDirectory: File, files: MutableList<File>) {
-        for (file in parentDirectory.listFiles()!!) {
-            if (file.name.endsWith(".java"))
-                files.add(file)
-            else if (file.isDirectory)
+        val directoryFiles = parentDirectory.listFiles() ?: emptyArray()
+        for (file in directoryFiles) {
+            if (file.isDirectory)
                 findJavaFiles(file, files)
+            else if (file.name.endsWith(".java"))
+                files.add(file)
         }
     }
 
     fun analyzeFile(javaFile: File) =
-        javaFile.forEachLine { measureLine(it) }
+        javaFile.readLines().forEachIndexed { lineCount, line -> analyzeLine(lineCount, line) }
 
-    private fun measureLine(line: String) {
-        lineCount++
-        val lineSize = line.length
-        totalChars += lineSize
-        lineWidthHistogram.addLine(lineSize, lineCount)
-        recordWidestLine(lineSize)
+    private fun analyzeLine(lineCount: Int, line: String) {
+        totalChars += line.length
+        lineWidthHistogram.addLine(lineCount, line)
+        recordWidestLine(lineCount, line)
     }
 
-    private fun recordWidestLine(lineSize: Int) {
-        if (lineSize > maxLineWidth) {
-            maxLineWidth = lineSize
+    private fun recordWidestLine(lineCount: Int, line: String) {
+        if (line.length > maxLineWidth) {
+            maxLineWidth = line.length
             widestLineNumber = lineCount
         }
     }
 
-    fun getMedianLineWidth(): Int {
-        val sortedWidths = getSortedWidths()
-        var cumulativeLineCount = 0
-        for (width in sortedWidths) {
-            cumulativeLineCount += lineCountForWidth(width)
-            if (cumulativeLineCount > lineCount / 2) return width
-        }
-        throw Error("Cannot get here")
-    }
-
-    private fun lineCountForWidth(width: Int) =
-        lineWidthHistogram.getLinesForWidth(width).size
-
-    private fun getSortedWidths(): List<Int> =
-        lineWidthHistogram.getWidths().sorted()
+    fun calcMedianLineWidth(): Int =
+        if (sortedWidths.isNotEmpty())
+            sortedWidths[lineCount / 2]
+        else
+            throw Error("Cannot cacl median without lines")
 }
--- src/main/LineWidthHistogram.kt
+++ src/main/LineWidthHistogram.kt
@@ -2,9 +2,10 @@ package main
 
 class LineWidthHistogram {
     val linesWidth = mutableMapOf<Int, Int>()
+    val lineCount: Int get() = linesWidth.size
 
-    fun addLine(lineNumber: Int, lineWidth: Int) =
-        linesWidth.put(lineNumber, lineWidth)
+    fun addLine(lineNumber: Int, line: String) =
+        linesWidth.put(lineNumber, line.length)
 
     fun getLinesForWidth(width: Int): MutableList<Int> =
         linesWidth.filter { it.value == width }.map { it.key }.toMutableList()
